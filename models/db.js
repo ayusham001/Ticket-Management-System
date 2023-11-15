ecfb860c7268ddf7b42a15a03dac8e267d6e9794
Ayush Mishra <92002533+ayusham001@users.noreply.github.com>
2023-11-15 09:33:19 +0530

    Update db.js

--- models/db.js
+++ models/db.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 module.exports.init= async function(){
     await mongoose.connect(
-        'mongodb+srv://ayush001:uXSWzBaSg3fElwBB@cluster0.xnla8rj.mongodb.net/TicketManagementSystem?retryWrites=true&w=majority'
-        )
+        'Link of MongoDB URL"
+    )
 }
